8d24ff7dca2b278f56fe4a0b2686ee2d892de4de
Themaister <maister@archlinux.us>
2010-05-20 04:48:28 +0200

    Forgot endian.c

---
+++ src/endian.c
@@ -0,0 +1,37 @@
+/*  RSound - A PCM audio client/server
+ *  Copyright (C) 2010 - Hans-Kristian Arntzen
+ * 
+ *  RSound is free software: you can redistribute it and/or modify it under the terms
+ *  of the GNU General Public License as published by the Free Software Found-
+ *  ation, either version 3 of the License, or (at your option) any later version.
+ *
+ *  RSound is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *  PURPOSE.  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with RSound.
+ *  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "endian.h"
+
+int is_little_endian(void)
+{
+   uint16_t i = 1;
+   return *((uint8_t*)&i);
+}
+
+void swap_endian_16 ( uint16_t* x )
+{
+   *x = (*x>>8) | (*x<<8);
+}
+
+void swap_endian_32 ( uint32_t* x )
+{
+   *x = (*x>>24) | 
+      ((*x<<8) & 0x00FF0000) |
+      ((*x>>8) & 0x0000FF00) |
+      (*x<<24);
+}
+
+
