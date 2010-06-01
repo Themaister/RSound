8735ce7a8ac8af0aff1aabe556ecb4e275753adf
Themaister <maister@archlinux.us>
2010-06-01 20:23:41 +0200

    :x

--- src/config.mk
+++ src/config.mk
@@ -1,6 +1,6 @@
 
-CC = cc
-CFLAGS = 
+CC = clang
+CFLAGS = -O2
 HAVE_ALSA = 1
 HAVE_OSS = 1
 HAVE_LIBAO = 1
@@ -9,4 +9,4 @@ HAVE_MUROAR = 1
 HAVE_AL = 1
 HAVE_PORTAUDIO = 1
 NEED_RT = 1
-PREFIX = /usr/local
+PREFIX = /usr
