04ab73b0021884e5f846c10f469e102f995fb891
Themaister <maister@archlinux.us>
2010-08-28 13:35:17 +0200

    Testing for a definite deadlock fix.

--- src/librsound/librsound.c
+++ src/librsound/librsound.c
@@ -793,21 +793,12 @@ static size_t rsnd_fill_buffer(rsound_t *rd, const char *buf, size_t size)
       }
       pthread_mutex_unlock(&rd->thread.mutex);
 
-#ifndef _WIN32
-      struct timespec tv;
-      clock_gettime(CLOCK_REALTIME, &tv);
-      tv.tv_sec += 1;
-#endif
-
       /* Sleeps until we can write to the FIFO. */
       //RSD_DEBUG("fill_buffer: going to sleep.");
-      pthread_cond_signal(&rd->thread.cond);
       pthread_mutex_lock(&rd->thread.cond_mutex);
-#ifdef _WIN32
+      pthread_cond_signal(&rd->thread.cond);
+
       pthread_cond_wait(&rd->thread.cond, &rd->thread.cond_mutex);
-#else
-      pthread_cond_timedwait(&rd->thread.cond, &rd->thread.cond_mutex, &tv);
-#endif
       pthread_mutex_unlock(&rd->thread.cond_mutex);
       //RSD_DEBUG("fill_buffer: Woke up.");
    }
@@ -1189,20 +1180,12 @@ static void* rsnd_thread ( void * thread_data )
          // There is a very slim change of getting a deadlock using the cond_wait scheme.
          // This solution is rather dirty, but avoids complete deadlocks at the very least.
 
-#ifndef _WIN32
-         struct timespec tv;
-         clock_gettime(CLOCK_REALTIME, &tv);
-         tv.tv_sec += 1;
-#endif
-
          RSD_DEBUG("Thread going to sleep.");
-         pthread_cond_signal(&rd->thread.cond);
          pthread_mutex_lock(&rd->thread.cond_mutex);
-#ifdef _WIN32
+         
+         pthread_cond_signal(&rd->thread.cond);
          pthread_cond_wait(&rd->thread.cond, &rd->thread.cond_mutex);
-#else
-         pthread_cond_timedwait(&rd->thread.cond, &rd->thread.cond_mutex, &tv);
-#endif
+
          RSD_DEBUG("Thread woke up.");
          pthread_mutex_unlock(&rd->thread.cond_mutex);
          RSD_DEBUG("Thread unlocked cond_mutex.");
