eb1ddefe64cda20daafdd9c76ff8ca61d81ab76c
Themaister <maister@archlinux.us>
2011-02-04 08:24:19 -0600

    Fix MacOSX build some more.

--- src/drivers/al.h
+++ src/drivers/al.h
@@ -18,8 +18,14 @@
 
 #include "../audio.h"
 #include "../endian.h"
+
+#ifdef __APPLE__
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#else
 #include <AL/al.h>
 #include <AL/alc.h>
+#endif
 
 typedef struct
 {
