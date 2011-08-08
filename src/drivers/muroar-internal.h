94d029f3a582afb963959982a2e6ca105bcf543b
Themaister <maister@archlinux.us>
2011-08-08 13:56:14 +0200

    Update copyrights.

--- src/drivers/muroar-internal.h
+++ src/drivers/muroar-internal.h
@@ -1,5 +1,5 @@
 /*  RSound - A PCM audio client/server
- *  Copyright (C) 2010 - Hans-Kristian Arntzen
+ *  Copyright (C) 2010-2011 - Hans-Kristian Arntzen
  *  Copyright (C) 2010 - Philipp Schafft
  * 
  *  RSound is free software: you can redistribute it and/or modify it under the terms
